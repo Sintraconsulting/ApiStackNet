caaa341e938b6c9d15869f93693161ad0f84b639
Riccardo Tinì <r.tini@sintraconsulting.eu>
2019-07-01 15:57:10 +0200

    - creazione test CRUD per "Product"; - aggiornato README;

---
+++ UnitTestProject2/ProductCRUDTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xunit;
+using ApiStackNet.Demo;
+using ApiStackNet.Demo.Entities;
+using ApiStackNet.Demo.BO;
+using ApiStackNet.Demo.BLL.Services;
+using ApiStackNet.Demo.DTO;
+using System.Collections.Generic;
+using ApiStackNetXUnitTest;
+using System.Linq;
+using System.Web.Http;
+
+namespace ApiStackNetDemoTest
+{
+    public class ProductCRUDTest : BaseTest
+    {
+        private ProductBO ProductBOCreate()
+        {
+            ProductBO product = new ProductBO()
+            {
+                ProductId = 001,
+                Desc = "Product test",
+                Price = 23.99
+            };
+
+            return product;
+        }
+
+        [Fact]
+        public void CRUDTest()
+        {
+            //Old BO
+            ProductBO productBO = ProductBOCreate();
+
+            //SAVE
+            ProductDTO ProductDTO = ProductService.SaveProduct(productBO);
+
+            //New BO
+            productBO.Id = ProductDTO.Id;
+            productBO.Desc = "Product test modified";
+
+            //EDIT         
+            ProductService.EditProduct(productBO);
+
+            //DELETE by Id
+            ProductService.Delete(ProductDTO.Id);
+
+            //Restore "Old BO" and save the Entity
+            productBO = ProductBOCreate();
+            ProductDTO = ProductService.SaveProduct(productBO);
+
+            //GET BY ID
+            ProductDTO = ProductService.GetById(ProductDTO.Id);
+
+            //DELETE by Entity
+            ProductService.Delete(ProductDTO);
+
+            //LIST
+            var list = ProductService.GetProductsList();
+        }
+    }
+}
--- UnitTestProject2/README.md
+++ UnitTestProject2/README.md
@@ -1,5 +1,8 @@
-﻿## 1)DemoTest
+﻿## DemoTest
+To run the CRUD test to control the behaviour of the library, we need:
+- to proceed throgh break points on every method call;
+- to check directly on the database if the results correspond to the behaviour we want the library has; 
 
-## infos
+## Infos
 The current test ambient refers to the "ApiStackNet.Demo" Project. 
 The current dependencies and NuGet packages are alligned with the "ApiStackNetTest" Project.
